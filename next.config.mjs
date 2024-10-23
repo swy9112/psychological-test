3fff7616729fca41929c455081eb19173352ab26
swy9112 <swy9112@naver.com>
2024-10-23 13:18:05 +0900

    파일 업로드

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  assetPrefix: isProd ? '/psychological-test/' : '',
   output: 'export',
   basePath: "/psychological-test",
 };
