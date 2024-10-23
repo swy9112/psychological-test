b9bd9957a6541419df53505c2361fcaa0b092fe5
swy9112 <swy9112@naver.com>
2024-10-23 11:18:41 +0900

    파일 업로드

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  assetPrefix: isProd ? '/psychological-test/' : '',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  basePath: "/psychological-test",
 };
+
+export default nextConfig;
