13ccf75f5fcd57bc4f97b554e6f359da69022800
swy9112 <swy9112@naver.com>
2024-10-23 14:22:14 +0900

    파일 업로드

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <Container>
+      <h1 className="flex items-center flex-1 text-xl">캠핑장에서 당신은 어떤 유형?</h1>
       <Link href="/main" className="w-full mt-auto px-2 py-4 bg-black text-white text-center">시작하기</Link>
     </Container>
   );
