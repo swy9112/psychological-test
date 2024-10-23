b174a21fb20c456714496ff8a679a56d35cc85e5
swy9112 <swy9112@naver.com>
2024-10-23 13:47:24 +0900

    파일 업로드

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  basePath: "/psychological-test",
+  // basePath: "/psychological-test",
+  assetPrefix: isProd ? '/psychological-test/' : '',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
+
